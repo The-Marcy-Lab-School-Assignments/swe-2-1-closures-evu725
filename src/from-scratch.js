8ff07798c92524c72ad5dcb891cb9b56b45ce858
Emily Vu <evu725@gmail.com>
2025-11-03 21:28:54 -0500

    finished problems 1-2

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,9 +1,27 @@
 const makeIdFunc = () => {
+  let count = 0;
 
+  const inner = () => {
+    count++;
+    return count;
+  };
+  return inner;
 };
 
 const makePasswordChecker = (correctPassword) => {
+  let attempts = 0;
 
+  const validPassword = (guess) => {
+    if (attempts >= 3) {
+      return 'Account locked';
+    }
+    if (guess === correctPassword) {
+      return true;
+    }
+    attempts++;
+    return false;
+  };
+  return validPassword;
 };
 
 const makeMultiplier = (multiplier) => {
